8e4bd7ac9a469c684dfc5df9c53da5f3f18b9b7f
DylanUpchr <dylan.upchr@eduge.ch>
2021-10-24 21:26:45 +0200

    Added file and function headers, cleaned code, corrected window size

--- src/dm.rs
+++ src/dm.rs
@@ -1,3 +1,8 @@
+/**
+ * Author:		Dylan Upchurch
+ * Date:		2021-10-24
+ * Desc:		Blackrust main crate (main entry point and opens webview)
+ */
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
@@ -5,17 +10,28 @@ extern crate image_base64;
 use web_view::*;
 use regex::Regex;
 use regex::Captures;
-//use xrandr::*;
 
+/**
+ * Name:	main
+ * Purpose:	Main entry point for program
+ * Args:	None
+ * Returns:	None
+ */
 fn main() {
 	open_webview();
 }
 
+/**
+ * Name:	open_webview
+ * Purpose:	Defines webview and opens it
+ * Args:	None
+ * Returns:	None
+ */
 fn open_webview() {
 	let html = combined_html_css_js();
 	let mut webview = web_view::builder()
 		.content(Content::Html(html))
-		.size(1260, 720)
+		.size(1280, 720)
 		.frameless(true)
 		.debug(true)
 		.user_data("")
@@ -35,6 +51,12 @@ fn open_webview() {
 	webview.run().unwrap();
 }
 
+/**
+ * Name:	combined_html_css_js
+ * Purpose:	Combines formatted HTML, CSS and JS all in one string
+ * Args:	None
+ * Returns:	(String) HTML webpage including CSS and JS
+ */
 fn combined_html_css_js() -> String {
 	format!(
 		"{}{}{}",
@@ -44,27 +66,45 @@ fn combined_html_css_js() -> String {
 	)
 }
 
+/**
+ * Name:	base64_encode_images
+ * Purpose:	Encode all references to images in an HTML webpage to base64 by path
+ * Args:	(&str) HTML webpage with image paths in src attributes
+ * Returns:	(String) HTML webpage with base64 image strings in src attributes
+ */
 fn base64_encode_images(html: &str) -> String {
 	let web_dir_prefix = "./src/web/";
     let re = Regex::new(r"(\./img/.*\.png)").unwrap();
     let result = re.replace_all(html, |caps: &Captures| {
-		println!("{}", &caps[0]);
         format!("{}", image_base64::to_base64(&format!("{}{}", web_dir_prefix, &caps[0])))
     });
 	return result.to_string();
 }
 
-fn inline_style(s: &str) -> String {
-	format!(r#"<style type="text/css">{}</style>"#, s)
+/**
+ * Name:	inline_style
+ * Purpose:	Surround CSS styles in HTML tag for inclusion in webpage
+ * Args:	(&str) CSS code
+ * Returns: (String) CSS code surrounded in HTML tag
+ */
+fn inline_style(css: &str) -> String {
+	format!(r#"<style type="text/css">{}</style>"#, css)
 }
 
-fn inline_script(s: &str) -> String {
-	format!(r#"<script type="text/javascript">{}</script>"#, s)
+/**
+ * Name:	inline_script
+ * Purpose:	Surround JS styles in HTML tag for inclusion in webpage
+ * Args:	(&str) JS code
+ * Returns: (String) JS code surrounded in HTML tag
+ */
+fn inline_script(js: &str) -> String {
+	format!(r#"<script type="text/javascript">{}</script>"#, js)
 }
 
 #[derive(Deserialize)]
 #[serde(tag = "cmd", rename_all = "camelCase")]
+//Enum defining commands that JS can invoke
 pub enum Cmd {
     Init,
-	Debug { value: String}
+	Debug { value: String }
 }
